df18bfa66b8c49de7b97550916e236d10904f4c1
XytoKie <vas.khomenko@gmail.com>
2024-01-15 17:56:13 +0200

    API for Categories

--- ExpenseTrackerRESTAPI/ExpenceTrackerRESTAPI.py
+++ ExpenseTrackerRESTAPI/ExpenceTrackerRESTAPI.py
@@ -43,6 +43,56 @@ def get_all_users():
     return jsonify(list(users.values()))
 
 
+@app.route('/category/<category_id>', methods=['GET'])
+def get_category(category_id):
+    int_id = int(category_id)
+    category = categories.get(int_id)
+    if category:
+        return jsonify(category)
+    return jsonify({'error': 'Category not found'}), 404
+
+
+@app.route('/category/<category_id>', methods=['DELETE'])
+def delete_category(category_id):
+    int_id = int(category_id)
+    if int_id in categories.keys():
+        del categories[int_id]
+        return jsonify({'message': 'Category deleted successfully'})
+    return jsonify({'error': 'Category not found'}), 404
+
+
+@app.route('/category', methods=['POST'])
+def create_category():
+    data = request.get_json()
+    category_id = int(data.get('id'))
+    name = data.get('name')
+
+    if category_id and name:
+        categories[category_id] = {'id': category_id, 'name': name}
+        return jsonify({'message': 'Category created successfully'})
+    return jsonify({'error': 'Invalid data'}), 400
+
+
+@app.route('/category', methods=['GET'])
+def get_all_categories():
+    return jsonify(list(categories.values()))
+
+
+
+@app.route('/record', methods=['GET'])
+def get_records():
+    user_id = int(request.args.get('user_id'))
+    category_id = int(request.args.get('category_id'))
+
+    if not user_id and not category_id:
+        return jsonify({'error': 'Missing parameters. Please provide user_id or category_id'}), 400
+
+    filtered_records = []
+    for record_id, record in records.items():
+        if (not user_id or record['user_id'] == user_id) and (not category_id or record['category_id'] == category_id):
+            filtered_records.append(record)
+
+    return jsonify(filtered_records)
 
 
 if __name__ == '__main__':
